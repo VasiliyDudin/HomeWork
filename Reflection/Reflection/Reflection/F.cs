71e52ddce9fe8d01023ff4d78980f0eadbc457eb
VasiliyDudin <vasdudin89@yandex.ru>
2022-12-28 22:59:00 +0300

    Правки ДЗ

--- Reflection/Reflection/Reflection/F.cs
+++ Reflection/Reflection/Reflection/F.cs
@@ -10,7 +10,7 @@ namespace Reflection
     class F
     {
         int i1, i2, i3, i4, i5;
-        public F Get() => new F() 
+        public static F Get() => new F() 
         { 
             i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 
         };
--- Reflection/Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Reflection/Program.cs
@@ -4,11 +4,18 @@
 
 using Reflection;
 using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization.Json;
 using System.Xml.Serialization;
 
-F test = new F();
+Type mytype = typeof(F);
+object test = Activator.CreateInstance(mytype);
+var method = mytype.GetMethods();
+string namemethod = method[0].Name;
+MethodInfo methodGet = mytype.GetMethod(namemethod);
+//var res = methodGet.Invoke(null, null);
+
 Stopwatch watch = new Stopwatch();
 
 FileStream stream = File.Create("test.dat");
@@ -18,13 +25,13 @@ for (int i=0; i < 10000; i++)
     formatter.Serialize(stream, test);
 watch.Stop();
 stream.Close();
-Console.WriteLine(test.Get());
+Console.WriteLine(methodGet.Invoke(null, null));
 Console.ReadLine();
 Console.WriteLine(watch.ElapsedMilliseconds);
 Console.ReadKey();
 stream = File.OpenRead("test.dat");
-test = formatter.Deserialize(stream) as F;
-Console.WriteLine("Имя    : " + test.Get());
+test = formatter.Deserialize(stream);
+Console.WriteLine("Имя    : " + methodGet.Invoke(null, null));
 stream.Close();
 Console.ReadKey();
 FileStream streamJS = File.Create("test.json");
@@ -38,8 +45,8 @@ watch.Start();
 streamJS = File.OpenRead("test.json");
 watch.Stop();
 Console.WriteLine(watch.ElapsedMilliseconds);
-test = formatterJS.ReadObject(streamJS) as F;
-Console.WriteLine(test.Get());
+test = formatterJS.ReadObject(streamJS);
+Console.WriteLine(methodGet.Invoke(null, null));
 streamJS.Close();
 
 Console.ReadKey();
