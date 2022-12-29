5eb131d515fdbf5fca77b21c43511ecf50dd5e67
VasiliyDudin <vasdudin89@yandex.ru>
2022-12-30 00:15:10 +0300

    Правки 2 ДЗ

--- Reflection/Reflection/Reflection/F.cs
+++ Reflection/Reflection/Reflection/F.cs
@@ -9,8 +9,8 @@ namespace Reflection
     [Serializable]
     class F
     {
-        int i1, i2, i3, i4, i5;
-        public static F Get() => new F() 
+        public int i1, i2, i3, i4, i5;
+        public F Get() => new F() 
         { 
             i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 
         };
--- Reflection/Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Reflection/Program.cs
@@ -7,16 +7,34 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization.Json;
-using System.Xml.Serialization;
-
-Type mytype = typeof(F);
-object test = Activator.CreateInstance(mytype);
-var method = mytype.GetMethods();
-string namemethod = method[0].Name;
-MethodInfo methodGet = mytype.GetMethod(namemethod);
-//var res = methodGet.Invoke(null, null);
 
 Stopwatch watch = new Stopwatch();
+Type mytype = typeof(F);
+F fs = new F();
+fs.i1 = 10;
+fs.i2 = 7;
+fs.i3 = 5;
+fs.i4 = 40;
+fs.i5 = 500;
+watch.Start();
+for (int i = 0; i < 10000; i++)
+    Serializer.Serialize(fs);
+watch.Stop();
+string result = Serializer.Serialize(fs);
+Console.WriteLine(result);
+Console.WriteLine("Serialize = " + watch.ElapsedMilliseconds);
+watch.Start();
+for (int i = 0; i < 10000; i++)
+    Serializer.Deserialize(result);
+watch.Stop();
+Console.WriteLine("Deserialize = " + watch.ElapsedMilliseconds);
+object test = Serializer.Deserialize(result);
+
+//object test = Activator.CreateInstance(mytype);
+//var method = mytype.GetMethods();
+//string namemethod = method[0].Name;
+//MethodInfo methodGet = mytype.GetMethod(namemethod);
+//var res = methodGet.Invoke(null, null);
 
 FileStream stream = File.Create("test.dat");
 BinaryFormatter formatter = new BinaryFormatter();
@@ -25,13 +43,14 @@ for (int i=0; i < 10000; i++)
     formatter.Serialize(stream, test);
 watch.Stop();
 stream.Close();
-Console.WriteLine(methodGet.Invoke(null, null));
-Console.ReadLine();
 Console.WriteLine(watch.ElapsedMilliseconds);
 Console.ReadKey();
 stream = File.OpenRead("test.dat");
+watch.Start();
 test = formatter.Deserialize(stream);
-Console.WriteLine("Имя    : " + methodGet.Invoke(null, null));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+//Console.WriteLine("Имя    : " + methodGet.Invoke(null, null));
 stream.Close();
 Console.ReadKey();
 FileStream streamJS = File.Create("test.json");
@@ -46,7 +65,7 @@ streamJS = File.OpenRead("test.json");
 watch.Stop();
 Console.WriteLine(watch.ElapsedMilliseconds);
 test = formatterJS.ReadObject(streamJS);
-Console.WriteLine(methodGet.Invoke(null, null));
+//Console.WriteLine(methodGet.Invoke(null, null));
 streamJS.Close();
 
 Console.ReadKey();
