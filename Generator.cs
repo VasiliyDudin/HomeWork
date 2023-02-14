aad099257e1f195c20778d6201203ab85020b96e
VasiliyDudin <117008684+VasiliyDudin@users.noreply.github.com>
2023-02-14 19:44:24 +0300

    Add files via upload

---
+++ Generator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Asinc
+{
+    internal class Generator
+    {
+        public const string _FileName = "GenerateFile.csv";
+        public const int _threadCount = 1000000;
+        static public void Run()
+        {
+            long phone = 79995008010;
+            var GenerateDatas = new List<DataF>();
+            Random rnd = new Random();
+            for (int i = 0; i < _threadCount; i++)
+            {
+                GenerateDatas.Add(new DataF(rnd.Next(1, _threadCount), "Ivanov Alex Sebastianos", $"test{i+1}@mail.ru", "+" + (phone + (_threadCount + i)).ToString()));
+            }
+
+            using (var writer = new StreamWriter(_FileName))
+            {
+                foreach(DataF dt in GenerateDatas)
+                {
+                    writer.WriteLine("ID: " + dt.ID);
+                    writer.WriteLine(dt.FIO);
+                    writer.WriteLine(dt.EMAIL);
+                    writer.WriteLine(dt.PHONE);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Asinc;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml.Linq;
+
+Console.WriteLine("Run generate fila by thread ?");
+if (Console.ReadKey().Key == ConsoleKey.Y)
+{
+    Thread th = new Thread(Generator.Run);
+    th.Start();
+    th.Join();
+}
+else
+{
+    Generator.Run();
+}
+
+List<string> linesF = new List<string>();
+List<Thread> threads = new List<Thread>();
+List<DataF> datas = new List<DataF>();
+int count = 0;
+
+/*using (StreamReader sr = File.OpenText(Generator._FileName))
+{
+    string s;
+    while ((s = sr.ReadLine()) != null)
+    {
+        linesF.Add(s);
+    }
+}*/
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+using (StreamReader reader = File.OpenText(Generator._FileName))
+{
+    string s;
+    while ((s = await reader.ReadLineAsync()) != null)
+    {
+        linesF.Add(s);
+    }
+}
+
+for (int i = 0; i < Generator._threadCount; i++)
+{
+    int CountRep = 0;
+    ThreadPool.QueueUserWorkItem(HandleInThreadPool, CountRep);
+    /*Thread thread = new Thread(() => { CountRep = OnGetLines(); });
+    threads.Add(thread);
+    threads[i].Start();
+    threads[i].Join();
+    while (CountRep > 0 && CountRep < 10)
+    {
+        Thread.Sleep(10);
+        threads[i].Start();
+    }
+    threads[i].Interrupt();*/
+}
+
+Thread.Sleep(10000);
+
+stopWatch.Stop();
+Console.ReadLine();
+
+async Task<int> OnGetLinesAsync()
+{
+    int result = 0;
+    try
+    {
+        string fio = String.Empty, email = String.Empty, phone = String.Empty;
+        int id = 0;
+        for (int i = count; i < linesF.Count; i++)
+        {
+            string line = linesF[i];
+            count++;
+            if (line.Contains("ID:") && id == 0)
+            {
+                id = Convert.ToInt32(line.Substring(4, line.Length - 4));
+                continue;
+            }
+            else if (string.IsNullOrEmpty(fio))
+            {
+                fio = line;
+                continue;
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                email = line;
+                continue;
+            }
+            else if (string.IsNullOrEmpty(phone))
+            {
+                phone = line;
+                continue;
+            }
+            else
+            {
+                count--;
+                break;
+            }
+        }
+
+        DataF df = new DataF(id, fio, email, phone);
+        datas.Add(df);
+        await DB.CreateCustomerAsync(df);
+    }
+    catch (Exception ex)
+    { 
+        Console.WriteLine(ex.Message);
+        result++;
+    }
+
+    return result;
+}
+
+void HandleInThreadPool(object item)
+{
+    int CountRep = (int)item;
+    ThreadPool.QueueUserWorkItem(HandleInThreadPool, CountRep);
+    var res = OnGetLinesAsync();
+}
